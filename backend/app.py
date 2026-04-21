acb7bde68700c2038e8e4622c5a250b960ac6d12
Anurag <yadav.anurag.tech@gmail.com>
2026-04-21 09:04:41 +0000

    Code Compiler DevOps project

---
+++ backend/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import subprocess
+import tempfile
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/run', methods=['POST'])
+def run_code():
+    code = request.json['code']
+
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".py") as temp:
+            temp.write(code.encode())
+            temp.flush()
+
+            result = subprocess.run(
+                ["python3", temp.name],
+                capture_output=True,
+                text=True,
+                timeout=5
+            )
+
+        return jsonify({
+            "output": result.stdout,
+            "error": result.stderr
+        })
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
